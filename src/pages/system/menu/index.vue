8786acfa0497c164a708667e90424d72fbc5d674
kuake <itzhongyi@foxmail.com>
2023-09-10 21:40:58 +0800

    feat:sparkles: 新增菜单编辑、添加、菜单列表刷新逻辑

--- src/pages/system/menu/index.vue
+++ src/pages/system/menu/index.vue
@@ -1,7 +1,17 @@
 <template>
 	<div class="main-box">
-		<Table @detailsEvent="detailsEvent" @editEvent="editEvent" />
-		<Edit :visible="editVisible" :data="editData" />
+		<Table
+			ref="tableRef"
+			@detailsEvent="detailsEvent"
+			@editEvent="editEvent"
+			@addEvent="addEvent"
+		/>
+		<Edit
+			v-model:visible="editVisible"
+			:data="editData"
+			:title="title"
+			@saveCallback="saveCallback"
+		/>
 		<Details v-model:visible="detailsVisible" :data="detailsData" />
 	</div>
 </template>
@@ -10,23 +20,44 @@ import { ref } from 'vue'
 import Table from './components/table.vue'
 import Edit from './components/edit.vue'
 import Details from './components/details.vue'
+import { SysMenu } from '@/types/models'
 
-const editVisible = ref(false)
-const editData = ref({})
-
+/**
+ * 菜单详情
+ */
 const detailsVisible = ref(false)
 const detailsData = ref({})
-
 const detailsEvent = (data: Object) => {
 	detailsVisible.value = true
-	console.log(data)
-	detailsData.value = data
+	detailsData.value = { ...data }
 }
 
-const editEvent = (data: Object) => {
+/**
+ * 编辑
+ */
+const title = ref('')
+const editVisible = ref(false)
+const editData = ref<SysMenu>()
+const editEvent = (data: SysMenu) => {
+	title.value = '编辑菜单'
+	editVisible.value = true
+	editData.value = { ...data }
+}
+
+/**
+ * 添加
+ */
+const addEvent = (data: SysMenu) => {
+	title.value = '添加菜单'
 	editVisible.value = true
 	editData.value = data
 }
+
+/**
+ * 保存回调函数，用于保存成功后，刷新菜单列表
+ */
+const tableRef = ref()
+const saveCallback = () => tableRef.value.refresh()
 </script>
 
 <style lang="scss" scoped>
