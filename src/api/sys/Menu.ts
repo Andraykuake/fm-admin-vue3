9c593301d5e1f81ec67aded64cd9f34d970ef490
kuake <itzhongyi@foxmail.com>
2023-09-24 20:30:00 +0800

    refactor:recycle: 删除接口文件中的前缀，在http拦截器中统一添加接口前缀

--- src/api/sys/Menu.ts
+++ src/api/sys/Menu.ts
@@ -1,34 +1,27 @@
-import {
-  get,
-  post,
-  put,
-  del
-} from '@/config/http.config'
+import { get, post, put, del } from '@/config/http.config'
 
 import { SysMenu } from '@/types/models'
 
-const baseURL = '/api/v1'
-
 export function deleteSysMenuById(id: number) {
-  return del(baseURL + '/menu/delete', { data: { id } })
+	return del('/menu/delete', { data: { id } })
 }
 
 export function querySysMenuList(data: any) {
-  return get(baseURL + '/menu/list', { params: data })
+	return get('/menu/list', { params: data })
 }
 
 export function querySysMenuById(data: any) {
-  return get(baseURL + '/menu/single', data)
+	return get('/menu/single', data)
 }
 
 export function querySysMenuByRoleId(data: any) {
-  return get(baseURL + '/menu/listByRoleId', data)
+	return get('/menu/listByRoleId', data)
 }
 
 export function addSysMenu(data: SysMenu) {
-  return post(baseURL + '/menu/add', data)
+	return post('/menu/add', data)
 }
 
 export function updateSysMenu(data: SysMenu) {
-  return put(baseURL + '/menu/update', data)
+	return put('/menu/update', data)
 }
--- src/api/sys/User.ts
+++ src/api/sys/User.ts
@@ -1,28 +1,21 @@
-import {
-  get,
-  post,
-  put,
-  del
-} from '@/config/http.config'
+import { get, post, put, del } from '@/config/http.config'
 import { SysUser } from '@/types/models'
 
-const baseURL = '/api/v1'
-
 export function deleteSysUserById(id: number) {
-  return del(baseURL + '/user/delete', { data: { id } })
+	return del('/user/delete', { data: { id } })
 }
 
 export function querySysUserList(data: any) {
-  return get(baseURL + '/user/list', { params: data })
+	return get('/user/list', { params: data })
 }
 
 export function querySysUserById(data: any) {
-  return post(baseURL + '/user/single', data)
+	return post('/user/single', data)
 }
 export function addSysUser(data: SysUser) {
-  return post(baseURL + '/user/add', data)
+	return post('/user/add', data)
 }
 
 export function updateSysUser(data: SysUser) {
-  return put(baseURL + '/user/update', data)
+	return put('/user/update', data)
 }
