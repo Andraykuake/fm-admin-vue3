21bc9796540fd55416af3d5db1b67e4b14ea5602
kuake <itzhongyi@foxmail.com>
2023-09-25 13:05:35 +0800

    refactor:recycle: 更新types models.ts 中的 SysMenu SysRole 的接口声明

--- src/types/models.ts
+++ src/types/models.ts
@@ -2,68 +2,66 @@
  * sys_menu
  */
 export interface SysMenu {
-    createBy?: string;
-    createTime?: string;
-    icon: string;
-    isRefresh?: string;
-    menuId?: number | string;
-    menuName: string;
-    menuType: string;
-    orderNum: number | string;
-    parentId: number | string;
-    perms: number | string;
-    remark?: string;
-    target?: number | string;
-    updateBy?: number | string;
-    updateTime?: number | string;
-    url: string;
-    visible: string;
-    children?: Array<SysMenu>
+	createBy?: string
+	createTime?: string
+	icon: string
+	isRefresh?: string
+	menuId?: number | string
+	menuName: string
+	menuType: string
+	orderNum: number | string
+	parentId: number | string
+	perms: number | string
+	remark?: string
+	target?: number | string
+	updateBy?: number | string
+	updateTime?: number | string
+	url: string
+	visible: string
+	children?: Array<SysMenu>
 }
 
 /**
  * sys_role
  */
 export interface SysRole {
-    create_by: string;
-    create_time: string;
-    data_scope: string;
-    del_flag: string;
-    remark: string;
-    role_id: number;
-    role_key: string;
-    role_name: string;
-    role_sort?: number;
-    status?: string;
-    update_by?: string;
-    update_time?: null;
+	createdBy?: string
+	createdAt?: string
+	dataScope: string
+	delFlag: string
+	remark: string
+	roleId?: number
+	roleKey: string
+	roleName: string
+	roleSort?: number
+	status?: string
+	updatedBy?: string
+	updatedAt?: null
 }
-
 
 /**
  * sys_user
  */
 export interface SysUser {
-    avatar?: string;
-    createBy?: string;
-    createTime?: Date | string;
-    delFlag: string;
-    deptId?: number | string;
-    email?: string;
-    lastLoginDate?: Date | string;
-    lastLoginIp?: string;
-    loginName: string;
-    password: string;
-    phoneNumber: string;
-    pwdLastUpdateDate?: Date | string;
-    remark?: string;
-    salt: string;
-    sex: string;
-    status: string;
-    updateBy?: string;
-    updateTime?: Date | string;
-    userId?: number | string;
-    userName: string;
-    userType: string;
+	avatar?: string
+	createBy?: string
+	createTime?: string
+	delFlag: string
+	deptId?: number | string
+	email?: string
+	lastLoginDate?: string
+	lastLoginIp?: string
+	loginName: string
+	password: string
+	phoneNumber: string
+	pwdLastUpdateDate?: string
+	remark?: string
+	salt: string
+	sex: string
+	status: string
+	updateBy?: string
+	updateTime?: string
+	userId?: number | string
+	userName: string
+	userType: string
 }
-
